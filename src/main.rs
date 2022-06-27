0d58050a0159937b842c0d0f3270e2131ca9d195
Mickey <hedgefund996@gmail.com>
2022-06-27 16:22:30 +0800

    plonkish && halo2 modular

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::marker::PhantomData;
+
+// 我们主要使用的module
+use halo2_proofs::{
+    // finite field
+    arithmetic::FieldExt,
+    // circuit和对table的操作
+    circuit::{AssignedCell, Chip, Layouter, Region, SimpleFloorPlanner, Value},
+    // 主要是lookup table
+    plonk::{Advice, Circuit, Column, ConstraintSystem, Error, Fixed, Instance, Selector},
+    // polynomial
+    poly::Rotation,
+};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! {:?}", );
 }
